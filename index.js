3459273188e9b8f59fe55a0fbed5c13bb83df5f7
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-10-13 14:10:27 -0400

    commit tree 6f97b3ff6cf41aec1fd3b6cc2ff055fb0b932b69

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const reId = document.getElementById("main")
+reId.remove();
+
+
+
+    const newHeader = document.createElement('h1')
+        newHeader.id = 'victory'
+
+    //const h1 = document.querySelector('h1')
+   // const newHeader = h1
+    //newHeader.id= 'victory'
+    newHeader.textContent = 'Elisa is the champion'
+
+    
